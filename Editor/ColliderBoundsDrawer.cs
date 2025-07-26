a5553a2b60445ddc1a8c97affa1d4c92a78c5fb3
Marius Gebhard <marius.wspi@gmail.com>
2025-07-27 00:27:31 +0200

    Add class to draw collider bounds

---
+++ Editor/ColliderBoundsDrawer.cs
@@ -0,0 +1,64 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UniUtils.Editor
+{
+    /// <summary>
+    /// A MonoBehaviour that visualizes the bounds of a Collider in the Unity Editor.
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class ColliderBoundsDrawer : MonoBehaviour
+    {
+        [SerializeField] private string label;
+        [SerializeField] private Color drawColor = Color.cyan;
+
+        private Collider colliderToVisualize;
+
+        private void OnDrawGizmos()
+        {
+#if UNITY_EDITOR
+            colliderToVisualize ??= GetComponent<Collider>();
+            if (!colliderToVisualize) return;
+
+            Gizmos.color = drawColor;
+            Vector3 center = colliderToVisualize.bounds.center;
+
+            switch (colliderToVisualize)
+            {
+                case BoxCollider box:
+                {
+                    Matrix4x4 matrix = Matrix4x4.TRS(
+                        transform.position,
+                        transform.rotation,
+                        transform.lossyScale
+                    );
+                    Gizmos.matrix = matrix;
+                    Gizmos.DrawWireCube(box.center, box.size);
+                    center = transform.TransformPoint(box.center);
+                    break;
+                }
+                case SphereCollider sphere:
+                    Gizmos.DrawWireSphere(
+                        transform.TransformPoint(sphere.center),
+                        sphere.radius * Mathf.Max(
+                            transform.lossyScale.x,
+                            transform.lossyScale.y,
+                            transform.lossyScale.z)
+                    );
+                    center = transform.TransformPoint(sphere.center);
+                    break;
+                default:
+                    Gizmos.DrawWireCube(colliderToVisualize.bounds.center, colliderToVisualize.bounds.size);
+                    break;
+            }
+
+            GUIStyle style = new()
+            {
+                normal = new GUIStyleState { textColor = drawColor },
+                fontStyle = FontStyle.Bold,
+            };
+            Handles.Label(center + Vector3.up * 0.25f, label, style);
+#endif
+        }
+    }
+}
